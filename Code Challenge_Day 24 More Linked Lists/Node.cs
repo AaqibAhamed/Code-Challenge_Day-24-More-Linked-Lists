0f471a90c87210c8827f864bab6e4ddc748bb8f0
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-21 07:53:27 +0530

    Add Node Class

---
+++ Code Challenge_Day 24 More Linked Lists/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_Challenge_Day_24_More_Linked_Lists
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
